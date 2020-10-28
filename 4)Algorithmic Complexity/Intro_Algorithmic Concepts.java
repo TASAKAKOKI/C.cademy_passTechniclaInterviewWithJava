79a4660f71c3ddaaa70c0756172a85e55ccbcc34
KOKI <agiks21@gmail.com>
2020-10-29 01:41:46 +0900

    Recursion_Conceptual

---
+++ 4)Algorithmic Complexity/Intro_Algorithmic Concepts.java
@@ -0,0 +1,7 @@
+Algorithmic Concepts
+        A brief overview of the algorithmic concepts to come.
+    Recursion
+        Have you ever thought about what would happen if you "called a function or method from within that function or method"? That’s called "recursion" and can actually "make your program more efficient". It can be used for problems that can be broken up into multiple steps, often ones that you would otherwise solve iteratively. It will also "be used in some of the traversal and sort algorithms" that you will see later!
+
+    Big O and Algorithmic Complexity
+        But how do you know when recursion is more efficient? "Time and space efficiency" are measured using "big O notation", which notes the "relative complexity of a section of code". Think of a single for loop versus nested for loops - which do you think is more time efficient? Or how about declaring an int versus an array of a thousand ints - which do you think takes more space?
