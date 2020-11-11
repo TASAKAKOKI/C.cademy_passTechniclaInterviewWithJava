9b4abc4819819dd02de386f0c719a4fc8b794024
KOKI <agiks21@gmail.com>
2020-11-11 20:43:47 +0900

    Technical Interview_ WhiteBoarding

---
+++ 8)Java Interview Prep and Algorithms Practice/1)Technical Interviews_ Whiteboarding/8)Review.java
@@ -0,0 +1,22 @@
+Review
+    Technical interviews judge the candidate’s ability to perform technical aspects of the job. "Whiteboarding" is a type of technical interview which "requires the candidate to code a solution without the assistance of a text editor".
+
+    Coding an efficient solution is important, but the ultimate goal as an interviewee is to "convince the interviewer you would be a desirable co-worker".
+
+    A "desirable co-worker" is "able to articulate their thought process and collaborate without friction".
+
+    Our whiteboarding strategies revolve around communication at every step.
+
+    The steps are:
+        1. Clarify the Problem
+        2. Create Inputs
+        3. Outline the Solution
+        4. Code the Solution
+        5. Test the Solution
+        6. Analyze the Solution
+
+Instructions
+    Practice makes perfect!
+
+    Run through these steps with a new practice problem:
+        Write a function which prints every number from 0 up to the given input. If divisible by 3, print "Fizz" instead of the number. If divisible by 5, print "Buzz". If input is divisible by 3 AND 5, print "FizzBuzz".
